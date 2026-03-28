f928e59bf77331fb51d5122665b99c2b4ca6ac86
PSantosss <josepaolo.santos.cics@ust.edu.ph>
2026-03-28 13:42:19 +0800

    vercel analytics

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import { routes } from "./routes";
 import SiteLayout from "../layout/SiteLayout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   const element = useRoutes(routes);
 
-  return <SiteLayout>{element}</SiteLayout>;
+  return (
+    <SiteLayout>
+      {element}
+      <Analytics />
+    </SiteLayout>
+  );
 }
